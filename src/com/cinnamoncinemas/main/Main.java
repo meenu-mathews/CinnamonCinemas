b05458a74c92f82bc15c4b90cb0d245af02754b8
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2022-02-14 13:04:29 +0000

    Get user input

--- src/com/cinnamoncinemas/main/Main.java
+++ src/com/cinnamoncinemas/main/Main.java
@@ -1,4 +1,13 @@
 package com.cinnamoncinemas.main;
 
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args){
+        Scanner scannerObj = new Scanner(System.in);
+        System.out.println("Enter number of seats (between 3 & 5)");
+        int numberOfSeats = scannerObj.nextInt();
+    }
 }
+
+
