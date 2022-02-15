836628d22e2a8886756a6d266a85badfa5de3835
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2022-02-15 19:22:05 +0000

    Unable to separate my concerns

--- src/com/cinnamoncinemas/main/SeatArray.java
+++ src/com/cinnamoncinemas/main/SeatArray.java
@@ -1,27 +1,8 @@
 package com.cinnamoncinemas.main;
 
 public class SeatArray {
-    private final int rows = 3;
-    private final int columns = 5;
-    private final int TOTALSEATS = rows*columns;
 
-    int numberOfSeats;
-    boolean[][] seatArray = new boolean[rows][columns];
 
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
 
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
 
-    public void allocateSeats(){
-        while(getNumberOfSeats()!=0)
-            for(int i=0;i<rows;i++){
-                for(int j=0;j<columns;j++){
-                    seatArray[i][j] = false;
-                }
-            }
-    }
 }
--- src/com/cinnamoncinemas/test/SeatsTest.java
+++ src/com/cinnamoncinemas/test/SeatsTest.java
@@ -4,17 +4,35 @@ import com.cinnamoncinemas.main.SeatArray;
 
 import java.util.Scanner;
 public class SeatsTest {
-    int seatsRemaining;
+    private final int rows = 3;
+    private final int columns = 5;
+    private final int TOTALSEATS = rows*columns;
+    boolean[][] seatArray = new boolean[rows][columns];
+    int numberOfSeats;
 
+    public int getNumberOfSeats(){
 
-    SeatArray seatArray = new SeatArray();
+        return TOTALSEATS-numberOfSeats;
+    }
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void allocateSeats(){
+        while(getNumberOfSeats()!=0)
+            for(int i=0;i<rows;i++){
+                for(int j=0;j<columns;j++){
+                    seatArray[i][j] = false;
+                }
+            }
+    }
     public boolean checkIfUserInputIsValid() {
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("Enter number of seats (between 1 & 3): ");
         } while( !isValid(numberOfSeats = in.nextInt()) );
-        seatArray.allocateSeats();
-        System.out.println("Only "+seatArray.getNumberOfSeats(numberOfSeats)+" empty seats remaining");
+        allocateSeats();
+        System.out.println("Only "+getNumberOfSeats()+" empty seats remaining");
         return true;
     }
 
